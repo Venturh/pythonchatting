d8c004d13c47c72a4785f48e921d76218f81fc5c
mwerp001 <venturh@github.com>
2019-05-28 16:10:43 +0200

    Gui überarbeitet:  -loginfenster hinzugefügt
     Userhandling:
     Login, Register und Logout  kann an Server gesendet werden (nach wiki befehlen)
     Client reagiert auf Antworten

--- Client.py
+++ Client.py
@@ -9,23 +9,11 @@ class Client:
     def __init__(self, server, port):
         self.server = server
         self.port = port
-        self.socket = None
-        self.connected = False
+        self.socket = socket(AF_INET, SOCK_STREAM)
+        self.connected = True
         self.gui = Gui(self)
         self.thread = None
-
-    def connect(self):
-        try:
-            self.socket = socket(AF_INET, SOCK_STREAM)
-            self.socket.connect((self.server, self.port))
-            self.gui.connect_button.destroy()
-        except socket.timeout:
-            print("Verbindung getrennt")
-            self.gui.connect_button.pack()
-        finally:
-            self.connected = True
-            self.thread = Thread(target=client.receive)
-            self.thread.start()
+        self.socket.connect((self.server, self.port))
 
     def receive(self):
         while True:
@@ -35,9 +23,8 @@ class Client:
             except OSError:
                 break
 
-    def send(self, event=None):
-        msg = self.gui.s_msg.get()
-        self.gui.s_msg.set("")
+    def send(self, msg):
+        print(msg)
         self.socket.send(bytes(msg + "\n", "utf-8"))
 
     def quit(self):
@@ -45,9 +32,58 @@ class Client:
             self.socket.close()
         self.gui.root.quit()
 
+    def send_txt(self, event=None):
+        msg = self.gui.s_msg.get()
+        self.gui.s_msg.set("")
+        self.send(msg)
+
+    def register(self):
+        user_msg = "RegU" + self.gui.register_user_msg.get()
+        pw_msg = "RegU" + self.gui.register_pw_msg.get()
+        self.send(user_msg)
+        self.send(pw_msg)
+
+        received = self.socket.recv(1024).decode("utf-8")
+        if received == "Nutzername bereits vergeben":
+            self.gui.info_label_msg.set("Benutzername ist schon vergeben!")
+        else:
+            self.gui.info_label_msg.set("Erfolgreich! Bitte einloggen")
+            self.gui.register_user_msg.set("")
+            self.gui.register_pw_msg.set("")
+
+    def login(self):
+        user_msg = "LoginU" + self.gui.login_user_msg.get()
+        pw_msg = "LoginU" + self.gui.login_pw_msg.get()
+        self.send(user_msg)
+        self.send(pw_msg)
+
+        received = self.socket.recv(1024).decode("utf-8")
+        if received == "Nutzername oder Passwort falsch":
+            self.gui.login_user_msg.set("")
+            self.gui.login_pw_msg.set("")
+            self.gui.info_label_msg.set("Falsche Anmeldedaten!")
+        elif received == "LoginU"+"\n":
+            self.gui.login_user_msg.set("")
+            self.gui.login_pw_msg.set("")
+            self.gui.info_label_msg.set("Keine Leeren Felder!")
+        else:
+            print("angemeldet")
+            self.gui.root.deiconify()
+            self.gui.top.destroy()
+            self.thread = Thread(target=client.receive)
+            self.thread.start()
+
+    def logout(self):
+        self.send("end")
+        self.gui.root.destroy()
+        self.thread.join()
+        sys.exit(0)
+
+
 
 if __name__ == "__main__":
     client = Client("localhost", 27999)
+
     mainloop()
 
 
--- Gui.py
+++ Gui.py
@@ -1,30 +1,74 @@
 from tkinter import *
+import sys
 
 
 class Gui(object):
-    def __init__(self, client):
+    def __init__(self,client):
         self.client = client
         self.root = Tk()
-        self.root.geometry('800x600')
+        self.root.withdraw()
+        self.top = Toplevel()
+        self.top.geometry("%dx%d%+d%+d" % (300, 300, 250, 125))
+
+        self.register_label = Label(self.top, text="Register here:")
+        self.register_user_msg = StringVar()
+        self.register_pw_msg = StringVar()
+        self.register_user = Entry(self.top, textvariable=self.register_user_msg)
+        self.register_pw = Entry(self.top,textvariable=self.register_pw_msg)
+        self.register_btn = Button(self.top, text="Register", command=self.register)
+        self.info_label_msg = StringVar()
+        self.info_label = Label(self.top, textvariable=self.info_label_msg)
+
+        self.login_label = Label(self.top, text="Login here:")
+        self.login_user_msg = StringVar()
+        self.login_pw_msg = StringVar()
+        self.login_user = Entry(self.top, textvariable=self.login_user_msg)
+        self.login_pw = Entry(self.top, textvariable=self.login_pw_msg)
+        self.login_btn = Button(self.top, text="Login", command=self.login)
+
+
+        self.close_btn = Button(self.top, text="Close", command=self.close_top)
+
+        self.register_label.pack()
+        self.register_user.pack()
+        self.register_pw.pack()
+        self.register_btn.pack()
+        self.info_label.pack()
+
+        self.login_label.pack()
+        self.login_user.pack()
+        self.login_pw.pack()
+        self.login_btn.pack()
+
+        self.close_btn.pack()
+
         self.chat_box = Frame()
 
         self.s_msg = StringVar()
         self.msg_list = Listbox(self.chat_box, height=30, width=80)
         self.chat_msg = Entry(textvariable=self.s_msg)
-        self.send_button = Button(text="Senden", command=client.send)
-        self.connect_button = Button(text="Verbiden", command=client.connect)
+        self.send_button = Button(text="Senden", command=self.client.send_txt)
+        self.logout_button = Button(text="Ausloggen", command=self.client.logout)
 
-        self.setup()
-
-    def setup(self):
-        self.root.title("Chat")
         self.root.protocol('WM_DELETE_WINDOW', self.client.quit)
-        self.connect_button.pack()
-
         self.msg_list.pack(fill=BOTH)
         self.chat_box.pack()
-
-        self.chat_msg.bind("<Return>", self.client.send)
+        self.chat_msg.bind("<Return>", self.client.send_txt)
         self.chat_msg.pack(fill=X)
-
         self.send_button.pack()
+        self.logout_button.pack()
+
+    def close_top(self):
+        self.top.destroy()
+        sys.exit()
+
+    def register(self):
+        print("registered")
+        self.client.register()
+
+    def login(self):
+        print("login")
+        self.client.login()
+
+
+
